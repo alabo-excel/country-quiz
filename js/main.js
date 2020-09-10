ba31414f621f3d2e42f00d3458102e38fc09bcdd
alabo-excel <iamalaboexcel@gmail.com>
2020-09-10 13:33:01 +0100

    option changes

--- js/main.js
+++ js/main.js
@@ -40,26 +40,43 @@ fetch("https://restcountries.eu/rest/v2/all")
     var correct = document.getElementById("green");
 
     correct.addEventListener("click", () => {
-        correct.style.backgroundColor = "green";
-        correct.style.border = "none";
-        correct.style.color = "white";
-        score = 1;
+        if (almost.style.backgroundColor === "orange" || wrong.style.backgroundColor === 'red') {
+            correct.style.backgroundColor = 'white'
+        } else {
+            correct.style.backgroundColor = "green";
+            correct.style.border = "none";
+            correct.style.color = "white";
+            score = 1;
+        }
+
     });
 
     var wrong = document.getElementById("wrong");
 
     wrong.addEventListener("click", () => {
-        wrong.style.backgroundColor = "red";
-        wrong.style.border = "none";
-        wrong.style.color = "white";
+        if (correct.style.backgroundColor === "green" || almost.style.backgroundColor === "orange") {
+            wrong.style.backgroundColor = 'white'
+        } else {
+            wrong.style.backgroundColor = "red";
+            wrong.style.border = "none";
+            wrong.style.color = "white";
+        }
     });
     var almost = document.getElementById("almost");
 
     almost.addEventListener("click", () => {
-        almost.style.backgroundColor = "orange";
-        almost.style.border = "none";
-        almost.style.color = "white";
+        if (correct.style.backgroundColor === "green" || wrong.style.backgroundColor === "red") {
+            almost.style.backgroundColor = "white"
+        } else {
+            almost.style.backgroundColor = "orange";
+            almost.style.border = "none";
+            almost.style.color = "white";
+        }
     });
+
+
+
+
     var next1 = document.getElementById("btn1");
     next1.addEventListener("click", () => {
         document.getElementById("container").innerHTML = ""
@@ -75,53 +92,65 @@ fetch("https://restcountries.eu/rest/v2/all")
             console.log(data[num].capital);
             document.getElementById("container").innerHTML += `
 
-    <div id="content" class="col-lg-4 col-sm-12 m-4 bg-white p-3">
-        <div class="mb-5 mt-5">
-            <h5>What is the capital city of ${data[num].name}</h5>
-        </div>
-
-        <div class="card p-2 mb-4"  id="almost">
-            <p>${data[num+1].capital}</p>
-        </div>
-        <div class="card p-2 mb-4"  id="wrong">
-            <p>${data[100].capital}</p>
-        </div>
-        <div class="card correct p-2 mb-4"  id="green">
-            <p>${data[num].capital}</p>
-        </div>
-        
-
-        <div class="p-2 mb-4"  id="btn2">
-            <p>Next</p>
-        </div>
+<div id="content" class="col-lg-4 col-sm-12 m-4 bg-white p-3">
+    <div class="mb-5 mt-5">
+        <h5>What is the capital city of ${data[num].name}</h5>
     </div>
-    
-    `;
+
+    <div class="card p-2 mb-4"  id="almost">
+        <p>${data[num+1].capital}</p>
+    </div>
+    <div class="card p-2 mb-4"  id="wrong">
+        <p>${data[100].capital}</p>
+    </div>
+    <div class="card correct p-2 mb-4"  id="green">
+        <p>${data[num].capital}</p>
+    </div>
+
+
+    <div class="p-2 mb-4"  id="btn2">
+        <p>Next</p>
+    </div>
+</div>
+
+`;
             spin.style.display = "none";
 
             var correct = document.getElementById("green");
 
             correct.addEventListener("click", () => {
-                correct.style.backgroundColor = "green";
-                correct.style.border = "none";
-                correct.style.color = "white";
-                score = score + 1;
+                if (almost.style.backgroundColor === "orange" || wrong.style.backgroundColor === 'red') {
+                    correct.style.backgroundColor = 'white'
+                } else {
+                    correct.style.backgroundColor = "green";
+                    correct.style.border = "none";
+                    correct.style.color = "white";
+                    score = 1;
+                }
 
             });
 
             var wrong = document.getElementById("wrong");
 
             wrong.addEventListener("click", () => {
-                wrong.style.backgroundColor = "red";
-                wrong.style.border = "none";
-                wrong.style.color = "white";
+                if (correct.style.backgroundColor === "green" || almost.style.backgroundColor === "orange") {
+                    wrong.style.backgroundColor = 'white'
+                } else {
+                    wrong.style.backgroundColor = "red";
+                    wrong.style.border = "none";
+                    wrong.style.color = "white";
+                }
             });
             var almost = document.getElementById("almost");
 
             almost.addEventListener("click", () => {
-                almost.style.backgroundColor = "orange";
-                almost.style.border = "none";
-                almost.style.color = "white";
+                if (correct.style.backgroundColor === "green" || wrong.style.backgroundColor === "red") {
+                    almost.style.backgroundColor = "white"
+                } else {
+                    almost.style.backgroundColor = "orange";
+                    almost.style.border = "none";
+                    almost.style.color = "white";
+                }
             });
 
             var next2 = document.getElementById("btn2");
@@ -139,56 +168,69 @@ fetch("https://restcountries.eu/rest/v2/all")
                     var num = Math.floor(Math.random() * 240 + 1);
                     console.log(data[num].capital);
                     document.getElementById("container").innerHTML += `
-            
-                <div id="content" class="col-lg-4 col-sm-12 m-4 bg-white p-3">
-                    <div class="mb-5 mt-5">
-                    <img src="${data[num].flag} ">
-                    <h5>is the national flag of what country</h5>
-                    </div>
-            
-                    <div class="card correct p-2 mb-4"  id="green">
-                        <p>${data[num].name}</p>
-                    </div>
-                    <div class="card p-2 mb-4"  id="wrong">
-                        <p>${data[100].name}</p>
-                    </div>
-                    
-                    <div class="card p-2 mb-4"  id="almost">
-                        <p>${data[num + 1].name}</p>
-                    </div>
-            
-                    <div class="p-2 mb-4"  id="btn3">
-                        <p>Next</p>
-                    </div>
+
+            <div id="content" class="col-lg-4 col-sm-12 m-4 bg-white p-3">
+                <div class="mb-5 mt-5">
+                <img src="${data[num].flag} ">
+                <h5>is the national flag of what country</h5>
                 </div>
-                
-                `;
+
+                <div class="card correct p-2 mb-4"  id="green">
+                    <p>${data[num].name}</p>
+                </div>
+                <div class="card p-2 mb-4"  id="wrong">
+                    <p>${data[100].name}</p>
+                </div>
+
+                <div class="card p-2 mb-4"  id="almost">
+                    <p>${data[num + 1].name}</p>
+                </div>
+
+                <div class="p-2 mb-4"  id="btn3">
+                    <p>Next</p>
+                </div>
+            </div>
+
+            `;
                     spin.style.display = "none";
 
                     var correct = document.getElementById("green");
 
                     correct.addEventListener("click", () => {
-                        correct.style.backgroundColor = "green";
-                        correct.style.border = "none";
-                        correct.style.color = "white";
-                        score = score + 1;
+                        if (almost.style.backgroundColor === "orange" || wrong.style.backgroundColor === 'red') {
+                            correct.style.backgroundColor = 'white'
+                        } else {
+                            correct.style.backgroundColor = "green";
+                            correct.style.border = "none";
+                            correct.style.color = "white";
+                            score = 1;
+                        }
 
                     });
 
                     var wrong = document.getElementById("wrong");
 
                     wrong.addEventListener("click", () => {
-                        wrong.style.backgroundColor = "red";
-                        wrong.style.border = "none";
-                        wrong.style.color = "white";
+                        if (correct.style.backgroundColor === "green" || almost.style.backgroundColor === "orange") {
+                            wrong.style.backgroundColor = 'white'
+                        } else {
+                            wrong.style.backgroundColor = "red";
+                            wrong.style.border = "none";
+                            wrong.style.color = "white";
+                        }
                     });
                     var almost = document.getElementById("almost");
 
                     almost.addEventListener("click", () => {
-                        almost.style.backgroundColor = "orange";
-                        almost.style.border = "none";
-                        almost.style.color = "white";
+                        if (correct.style.backgroundColor === "green" || wrong.style.backgroundColor === "red") {
+                            almost.style.backgroundColor = "white"
+                        } else {
+                            almost.style.backgroundColor = "orange";
+                            almost.style.border = "none";
+                            almost.style.color = "white";
+                        }
                     });
+
                     var next3 = document.getElementById("btn3");
                     next3.addEventListener("click", () => {
                         document.getElementById("container").innerHTML = "";
@@ -197,7 +239,7 @@ fetch("https://restcountries.eu/rest/v2/all")
 
 
                         document.getElementById("container").innerHTML += `
-            
+
                         <div id="content" class="col-lg-4 col-sm-12 m-4 bg-white p-3">
                            <div class="p-4 text-center">
                            <h3>
@@ -207,7 +249,7 @@ fetch("https://restcountries.eu/rest/v2/all")
                             ${score}
                            </h1>
                            </div>
-                    
+
                             <div class="p-2 mb-4" id="btn4">
                                 <p>Retake test</p>
                             </div>
